517d411e799e40b8dc05395700803b99a9ede11f
Chris Lee <chrislee@Bartswievvwzxqw.localdomain>
2022-05-12 13:12:51 -0400

    hidden btn

---
+++ app.js
@@ -0,0 +1,10 @@
+const playBtn = document.querySelector('.btn-play')
+const introPage  = document.querySelector('section')
+
+
+
+//BTN Starts the Game
+playBtn.addEventListener('click', function() {
+introPage.classList.add('hidden')
+    
+})
