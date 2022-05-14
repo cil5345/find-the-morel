4e3fe55e64088e4a88e092b60f982fe90f17e408
Chris Lee <chrislee@Chriss-MacBook-Pro-2.local>
2022-05-13 20:48:19 -0400

    made pic background

--- app.js
+++ app.js
@@ -3,10 +3,25 @@
 const playBtn = document.querySelector('.btn-play');
 const introPage = document.querySelector('section');
 const gamePage = document.querySelector('.game-page');
+const mushroom = document.querySelector('.mushroom');
+const forestFloor = document.querySelector('.forest-floor')
 
+
+
+console.log(document)
+console.log(window)
 //BTN Starts the Game
 playBtn.addEventListener('click', function () {
-  introPage.remove()
-  gamePage.classList.remove('viewport')
-
+  introPage.remove();
+  gamePage.classList.remove('viewport');
+ 
+  
 });
+
+//Randomize Mushroom Location within ppic
+const randomizeMush = function () {
+  
+  mushroom.style.top = `${x}px`
+  mushroom.style.bottom = `${y}px`
+  
+};
